cb0d48c4603bd1491eca8c438a0a69cb6e4d4ec6
Fernando Antunes <fernando.cardoso@imbel.gov.br>
2023-04-24 10:23:09 -0300

    print com dados primitivos

--- print.py
+++ print.py
@@ -1,3 +1,5 @@
-nome = 'Fernando'
+nome = "Fernando"
+altura = float(1.85)
+idade = int(31)
 
-print(f'Meu nome e {nome}!')
+print(f'Meu nome e {nome} e a altura é {altura} e a minha idade é {idade}!')
