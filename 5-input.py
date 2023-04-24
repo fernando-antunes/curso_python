1145015718e66fa674fc3151673d41f83dd1587c
Fernando Antunes <fernando.cardoso@imbel.gov.br>
2023-04-24 15:38:57 -0300

    input

---
+++ 5-input.py
@@ -0,0 +1,10 @@
+#Nome
+nome = input("Digite o seu nome: ")
+
+#Idade
+idade = int(input("Qual a sua idade: "))
+
+#Nascimento
+nascimento = 2023 - idade
+
+print(f"O nome é {nome} e a idade é {idade} e você nasceu em {nascimento}")
