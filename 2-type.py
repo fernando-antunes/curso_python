42e94c8ba0c6886687fe32d154c964b745506018
Fernando Antunes <fernando.cardoso@imbel.gov.br>
2023-04-24 11:07:02 -0300

    tipo de variaveis

---
+++ 2-type.py
@@ -0,0 +1,17 @@
+num1 = 6
+num2 = 2.9
+
+print(type(num1))
+print(type(num2))
+
+a = float(num1)
+print(a)
+print(type(a))
+
+b = int(num2)
+print(b)
+print(type(b))
+
+c = '3'
+print(c)
+print(type(c))
