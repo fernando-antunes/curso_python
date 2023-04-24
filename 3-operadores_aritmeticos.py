aeb057e94547a6f344113cef1b81a5030010fee7
Fernando Antunes <fernando.cardoso@imbel.gov.br>
2023-04-24 13:57:45 -0300

    operadores aritmeticos

---
+++ 3-operadores_aritmeticos.py
@@ -0,0 +1,41 @@
+import math
+
+num1 = 10
+num2 = 2
+
+print(num1 + num2) #soma
+print(num1 - num2) #subtração
+print(num1 * num2) #multiplicação
+print(num1 / num2) #divisão
+print(num1 % num2) #resto da divisao
+print(num1 ** num2) #elevado a 
+print(num1 // num2) #divisao sem casas decimais
+
+# operação complexa
+print((3 + 5) * (7 + 3))
+
+#valor absoluto
+print(abs(-15))
+
+#elevado
+print(pow(3,3))
+
+#maior numero
+print(max(1,6,7,43,0))
+
+#menor numero
+print(min(1,6,7,43,0))
+
+#arredondamento
+print(round(8.8))
+
+##importacao math##
+
+#arredondamento para baixo
+print(math.floor(8.8))
+
+#arredondamento para cima
+print(math.ceil(8.0000000001))
+
+#raiz quadrada
+print(math.sqrt(9))
